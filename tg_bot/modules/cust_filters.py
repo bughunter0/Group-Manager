96d479c4abe8661e85995c78a73c6b5f232684b8
BUGHUNTER <62880706+bughunter0@users.noreply.github.com>
2020-09-01 16:47:25 +0530

    Update cust_filters.py

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -9,16 +9,16 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, DispatcherHandlerStop, run_async
 from telegram.utils.helpers import escape_markdown
 
-from miakhalifa import dispatcher, LOGGER, FILTER_LIMIT
-from miakhalifa.modules.disable import DisableAbleCommandHandler
-from miakhalifa.modules.helper_funcs.chat_status import user_admin
-from miakhalifa.modules.helper_funcs.extraction import extract_text
-from miakhalifa.modules.helper_funcs.filters import CustomFilters
-from miakhalifa.modules.helper_funcs.misc import build_keyboard
-from miakhalifa.modules.helper_funcs.string_handling import split_quotes, button_markdown_parser
-from miakhalifa.modules.sql import cust_filters_sql as sql
+from tg_bot import dispatcher, LOGGER, FILTER_LIMIT
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot.modules.helper_funcs.chat_status import user_admin
+from tg_bot.modules.helper_funcs.extraction import extract_text
+from tg_bot.modules.helper_funcs.filters import CustomFilters
+from tg_bot.modules.helper_funcs.misc import build_keyboard
+from tg_bot.modules.helper_funcs.string_handling import split_quotes, button_markdown_parser
+from tg_bot.modules.sql import cust_filters_sql as sql
 
-from miakhalifa.modules.connection import connected
+from tg_bot.modules.connection import connected
 
 HANDLER_GROUP = 10
 BASIC_FILTER_STRING = "*Filters in this chat:*\n"
