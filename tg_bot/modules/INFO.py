12491edb08a971ce89cdd843836dd236a6953f9a
BUGHUNTER <62880706+bughunter0@users.noreply.github.com>
2020-09-01 17:39:53 +0530

    Add files via upload

---
+++ tg_bot/modules/INFO.py
@@ -0,0 +1,13 @@
+
+__help__ = """
+◆ Bot Name : *Filter Adder*
+◆ Creator : [bughunter0](http://t.me//bughunter0)
+◆ Credits :  @Noob_admin
+◆ Language : Python3
+◆ Source Code : 👉 @bughunterbots
+◆ Server : Heroku
+◆ Database : SQL
+◆ Build Status : Beta
+"""
+__mod_name__ = "🔺About🔺"
+
